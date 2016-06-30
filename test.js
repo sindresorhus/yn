ca560c174f32e4bce219d14bb6b7172ac068870c
Andrew Huff <andrewhuff4@gmail.com>
2016-06-29 21:29:16 -0600

    add tests
    * Adds null tests
    * Adds tests for '1' and '0' and capitalizations of 'true' and 'false'

--- test.js
+++ test.js
@@ -9,7 +9,10 @@ test('truthy cases', t => {
 		'YES',
 		'Yes',
 		'true',
+		'TRUE',
+		'True',
 		true,
+		'1',
 		1
 	].forEach(el => {
 		t.true(yn(el));
@@ -25,7 +28,10 @@ test('falsey cases', t => {
 		'NO',
 		'No',
 		'false',
+		'FALSE',
+		'False',
 		false,
+		'0',
 		0
 	].forEach(el => {
 		t.false(yn(el));
@@ -33,15 +39,35 @@ test('falsey cases', t => {
 	});
 });
 
-test('edge cases which return null', t => {
-	t.is(yn(NaN), null);
-	t.is(yn(''), null);
-	t.is(yn('yn'), null);
-	t.is(yn('unicorn'), null);
-	t.is(yn(NaN, {lenient: true}), null);
-	t.is(yn('', {lenient: true}), null);
-	t.is(yn('yn', {lenient: true}), null);
-	t.is(yn('unicorn', {lenient: true}), null);
+test('null cases', t => {
+	[
+	// Falsey cases that don't work
+		NaN,
+		null,
+		undefined,
+		'',
+		[],
+		{},
+	// Numbers: only works on 0 and 1
+		'10',
+		10,
+		'-1',
+		-1,
+		'0.5',
+		0.5,
+		'1BadIntParsing',
+		'0x000',
+	// Strings with a low edit-distance don't work
+		'flase',
+		'ture',
+		'n o',
+		'yn',
+	// Other
+		'unicorn'
+	].forEach(el => {
+		t.is(null, yn(el));
+		t.is(null, yn(el, {lenient: true}));
+	});
 });
 
 test('lenient option - truthy value cases', t => {
