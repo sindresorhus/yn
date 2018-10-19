942b49e90935f882cef9504316fa48537beab5f4
Lorens Leon <lorensleon@gmail.com>
2018-10-19 19:47:04 +0200

    chore: apply style guide

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,24 @@
-export interface YesNoOptions<T = null> {
+export interface Options<Default = null> {
+	/**
+	 * Use a key distance-based score to leniently accept typos of `yes` and `no`
+	 *
+	 * @default false
+	 */
 	lenient?: boolean;
-	default?: boolean | T;
+
+	/**
+	 * Default value if no match was found
+	 *
+	 * @default null
+	 */
+	default?: boolean | Default;
 }
 
 /**
  * Parse yes/no like values
  * The following case-insensitive values are recognized: `'y', 'yes', 'true', true, '1', 1, 'n', 'no', 'false', false, '0', 0`
  *
- * @param input Value that should be converted
- * @param [options] Parsing options
- * @param [options.lenient=false] Use a key distance-based score to leniently accept typos of `yes` and `no`
- * @param [options.default=null] Default value if no match was found
+ * @param input - Value that should be converted
+ * @return The parsed input. A boolean if it could be parsed or the given default if not
  */
-export default function yn<T>(input: any, options?: YesNoOptions<T>): boolean | T;
+export default function yn<Default = null>(input: any, options?: Options<Default>): boolean | Default;
