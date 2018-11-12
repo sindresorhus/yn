577f7f3a91a559323601d2dd5f5fade2e4c8c70c
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-13 01:17:26 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 export interface Options<Default = null> {
 	/**
-	 * Use a key distance-based score to leniently accept typos of `yes` and `no`
+	 * Use a key distance-based score to leniently accept typos of `yes` and `no`.
 	 *
 	 * @default false
 	 */
 	lenient?: boolean;
 
 	/**
-	 * Default value if no match was found
+	 * Default value if no match was found.
 	 *
 	 * @default null
 	 */
@@ -15,10 +15,10 @@ export interface Options<Default = null> {
 }
 
 /**
- * Parse yes/no like values
+ * Parse yes/no like values.
  * The following case-insensitive values are recognized: `'y', 'yes', 'true', true, '1', 1, 'n', 'no', 'false', false, '0', 0`
  *
- * @param input - Value that should be converted
- * @return The parsed input. A boolean if it could be parsed or the given default if not
+ * @param input - Value that should be converted.
+ * @returns The parsed input if it can be parsed or the default value defined in the `default` option.
  */
 export default function yn<Default = null>(input: any, options?: Options<Default>): boolean | Default;
