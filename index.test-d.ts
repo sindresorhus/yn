fd15f10d7290a560d14941271b033546fdb79e56
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-13 01:19:27 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,11 +1,11 @@
-import { expectType } from 'tsd-check';
+import {expectType} from 'tsd-check';
 import yn from '.';
 
-// should use null as default when no options are given
+// Should use null as default when no options are given
 expectType<boolean | null>(yn('true'));
 
-// should use the default type when given
+// Should use the default type when given
 expectType<boolean | number>(yn('true', { default: 1 }));
 
-// should use null as default when only the lenient option is given
+// Should use null as default when only the lenient option is given
 expectType<boolean | null>(yn('true', { lenient: true }));
